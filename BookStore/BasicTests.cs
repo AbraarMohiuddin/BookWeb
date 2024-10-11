fffb3b7bd5f1721b4e8a6a9294e9a3ed6dc15dc7
Abraar Mohiuddin <abraarmohiuddin3@outlook.com>
2024-10-11 17:00:10 +1100

    test

---
+++ BookStore/BasicTests.cs
@@ -0,0 +1,32 @@
+﻿// BasicTests.cs
+
+using NuGet.ContentModel;
+using Xunit;
+
+namespace BookStore
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void SimpleTest_ShouldPass()
+        {
+            // Arrange
+            int expected = 5;
+            int actual = 5;
+
+            // Act & Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SimpleTest_ShouldFail()
+        {
+            // Arrange
+            int expected = 5;
+            int actual = 3;
+
+            // Act & Assert
+            Assert.NotEqual(expected, actual);
+        }
+    }
+}
